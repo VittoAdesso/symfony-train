ffb231d7554e03b7e887439549c6ec32129c40f8
Vittorio Adesso <adessovittorio8@gmail.com>
2022-03-13 23:08:33 +0100

    created entity

---
+++ src/Entity/Solicitud.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\SolicitudRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: SolicitudRepository::class)]
+class Solicitud
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private $id;
+
+    #[ORM\Column(type: 'date')]
+    private $fechaInicio;
+
+    #[ORM\Column(type: 'date')]
+    private $fechafin;
+
+    #[ORM\Column(type: 'integer')]
+    private $numPaquetes;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getFechaInicio(): ?\DateTimeInterface
+    {
+        return $this->fechaInicio;
+    }
+
+    public function setFechaInicio(\DateTimeInterface $fechaInicio): self
+    {
+        $this->fechaInicio = $fechaInicio;
+
+        return $this;
+    }
+
+    public function getFechafin(): ?\DateTimeInterface
+    {
+        return $this->fechafin;
+    }
+
+    public function setFechafin(\DateTimeInterface $fechafin): self
+    {
+        $this->fechafin = $fechafin;
+
+        return $this;
+    }
+
+    public function getNumPaquetes(): ?int
+    {
+        return $this->numPaquetes;
+    }
+
+    public function setNumPaquetes(int $numPaquetes): self
+    {
+        $this->numPaquetes = $numPaquetes;
+
+        return $this;
+    }
+}
